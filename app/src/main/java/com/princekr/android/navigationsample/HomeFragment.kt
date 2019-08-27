0a22bcbce48e1060e9bb5878b3912b5f272a6dca
prince <prince18pat@gmail.com>
2019-08-27 20:32:01 +0900

    add fragments and navigations

--- app/src/main/java/com/princekr/android/navigationsample/HomeFragment.kt
+++ app/src/main/java/com/princekr/android/navigationsample/HomeFragment.kt
@@ -5,7 +5,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.navOptions
 
 /**
  * A simple [Fragment] subclass.
@@ -21,5 +24,20 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        val options = navOptions {
+            anim {
+                enter = R.anim.slide_in_right
+                exit = R.anim.slide_out_left
+                popEnter = R.anim.slide_in_left
+                popExit = R.anim.slide_out_right
+            }
+        }
+
+        view.findViewById<Button>(R.id.navigate_destination_button)?.setOnClickListener {
+            findNavController().navigate(R.id.flow_step_one_dest, null, options)
+        }
+    }
 }
--- app/src/main/java/com/princekr/android/navigationsample/MainActivity.kt
+++ app/src/main/java/com/princekr/android/navigationsample/MainActivity.kt
@@ -2,11 +2,42 @@ package com.princekr.android.navigationsample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 
+/**
+ * A simple Activity demonstrating use of a NavHostFragment with a navigation drawer.
+ */
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.navigation_activity)
+
+        var toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        var host: NavHostFragment =
+            supportFragmentManager.findFragmentById(R.id.my_nav_host_fragment)
+                    as NavHostFragment? ?: return
+
+        // set up action bar
+        val navController = host.navController
+
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        setupActionBar(navController, appBarConfiguration)
+    }
+
+    private fun setupActionBar(
+        navController: NavController,
+        appBarConfiguration: AppBarConfiguration
+    ) {
+
     }
 }
